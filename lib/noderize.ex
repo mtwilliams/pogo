b1556e863bd987d97614590d71c85233e24ca08b
Kurt Williams <kurt.r.williams@gmail.com>
2013-10-11 12:30:16 -0500

    baby steps towards start/stop script

--- lib/noderize.ex
+++ lib/noderize.ex
@@ -1,49 +1,44 @@
 defmodule Noderize do
+	def alive?(node) when is_binary(node) do
+		node |> binary_to_atom |> alive?
+	end
 	def alive?(node) do
-		unless is_alive?(node) do
-			IO.puts node_down(node)
-			halt(1)
-		end
-		IO.puts "#{node} is alive"
-		halt(0)
+		:pong == :net_adm.ping(node)
 	end
 
-	defp halt(status), do: :erlang.halt status
-
-	def stop(node, timeout // 2000) do
-		unless is_alive?(node) do
-			IO.puts node_down(node)
-			halt(1)
-		end
-		stop_node(node, timeout)
+	def alive!(node) do
+		unless alive?(node), do: exit(1)
+		exit(0)
 	end
 
-	def stop_node(node, timeout) do
+	def stop(node) when is_binary(node) do
+		stop(node, 2000)
+	end
+	def stop(node, timeout) when is_binary(node) do
+		node |> binary_to_atom |> stop(timeout)
+	end
+	def stop(node, timeout) do
 		case :rpc.call(node, :init, :stop, []) do
 			:ok ->
 				wait_for_node(node, timeout)
 				halt(0)
 			{ :badrpc, :nodedown } ->
-				IO.puts node_down(node)
 				halt(1)
 		end
 	end
 
-	defp is_alive?(node) do
-		:pong == :net_adm.ping(node)
-	end
+	defp halt(status), do: :erlang.halt status
 
 	defp wait_for_node(node, timeout) do
 		wait_for_node(node, 1, 0, timeout)
 	end
 
-	defp wait_for_node(node, _, acc_wait, timeout) when acc_wait >= timeout do
-		IO.puts "#{node} timed out"
+	defp wait_for_node(_, _, acc_wait, timeout) when acc_wait >= timeout do
 		halt(1)
 	end
 
 	defp wait_for_node(node, interval, acc_wait, timeout) do
-		if is_alive?(node) do
+		if alive?(node) do
 			ref = :erlang.make_ref
 			self = Process.self
 			:erlang.send_after(interval, self, { self, ref })
@@ -52,12 +47,6 @@ defmodule Noderize do
 					acc_wait = acc_wait + interval
 					wait_for_node(node, interval, acc_wait, timeout)
 			end
-		else
-			IO.puts "#{node} stopped"
 		end
-	end
-
-	defp node_down(node) do
-		"#{node} is unreachable"
 	end
 end
