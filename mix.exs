2a7cdecc69bf1e82d862b9201b130e084d21306a
Kurt Williams <kurt.r.williams@gmail.com>
2013-10-14 13:19:54 -0500

    go pogo...

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Noderize.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :noderize,
+    [ app: :pogo,
       version: "0.0.1",
       elixir: "~> 0.10.3",
       deps: deps ]
