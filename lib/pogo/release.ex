b1a53761b616ce3175ef8c46ff3c4ce200d74979
Devin Torres <devin@devintorr.es>
2013-10-17 13:20:24 -0500

    Fix chmod on the genereted script

--- lib/pogo/release.ex
+++ lib/pogo/release.ex
@@ -20,6 +20,6 @@ defmodule Pogo.Release do
     tmpl = Path.join([ :code.priv_dir(:pogo), "bin", "script.eex" ])
     unless File.exists?(bin_path = Path.dirname(path)), do: File.mkdir_p!(bin_path)
     File.write(path, EEx.eval_file(tmpl, app: app))
-    File.chmod(path, 0544)
+    File.chmod(path, 0755)
   end
 end
